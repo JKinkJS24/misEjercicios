558c7e149b75c0b3ade5e91ef5f5a33f031b7b87
JkinkJS <jkinkjs24@outlook.com>
2024-07-18 20:46:50 +0200

    ejercicioquenitanmalperopodríaestarmejor

--- ejercicio026_excepciones2/ejercicio.js
+++ ejercicio026_excepciones2/ejercicio.js
@@ -1,10 +1,31 @@
 let paises = Array("Islandia", "Finlandia", "Suecia", "Noruega", "Groenlandia")
 
 function organizarViaje(pais, pax, dias) {
-    if (pais != paises) {
-        console.log("nope")
+
+    if (paises.includes(pais)) {
+        console.log("País Ok");
     } else {
-        console.log("Todo oki")
+        throw new Error("Ese país no está disponible.");
     }
+
+    if (pax >= 1 && pax <= 4) {
+        console.log("Personas Ok");
+    } else {
+        throw new Error("Número de personas no disponible.");
+    }
+
+    if (dias >= 5 && dias <= 10) {
+        console.log("Días Ok");
+    } else {
+        throw new Error("Número de días no disponible");
+    }
+    console.log(`Se ha reservado un viaje a ${pais} para ${pax} durante ${dias}.`)
 }
-console.log(organizarViaje("Islandia", 2, 3))
+
+try {
+    console.log(organizarViaje("Islandia", 0, 6))
+} catch (e) {
+    console.error(e.message);
+} finally {
+    console.log("Proceso terminado.")
+}
